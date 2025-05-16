8f1280b1899e42cfd0d0b5d2e4322a2c43921813
Ayushi Anand <160775991+Or4cle404@users.noreply.github.com>
2025-05-16 16:10:58 +0530

    Update returned.c

--- returned.c
+++ returned.c
@@ -1,1 +1,13 @@
 
+#include <stdio.h>
+
+/*@returned@*/ char* echo(/*@notnull@*/ char *msg) {
+    return msg;  // Same pointer returned
+}
+
+int main() {
+    char *m = "Hello";
+    char *r = echo(m);
+    printf("%s\n", r);
+    return 0;
+}
